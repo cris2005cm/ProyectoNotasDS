0a682fe94736f81c5387373209408c38569025c6
Sebastian Cruz <APRENDIZ@BOGDAFCGFFSP145.FormacionSena.RED>
2023-06-28 17:36:07 -0500

    ELIMINAR

--- Administrador/pages/editar.php
+++ Administrador/pages/editar.php
@@ -1,7 +1,30 @@
-<!DOCTYPE html>
-<html>
-<link rel="stylesheet" type="text/css" href="../../css/aggadmins.css">
-<body>
+<html lang="en">
+<head>
+  <title>Agregar</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body style="background-image:  linear-gradient(100deg, #cbffdc 0, #baffdc 16.67%, #abffda 33.33%, #9df2d5 50%, #92e3cf 66.67%, #89d5c9 83.33%, #83c9c4 100%)">
+    <nav>
+        <ul class="nav">
+            <li class="nav-item" style="background-color: aliceblue; border: solid 1px;position: relative; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+                <a class="nav-link" href="#">Materias</a>
+            </li>
+            <li class="nav-item" style="background-color: aliceblue; border: solid 1px;position: relative; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+                <a class="nav-link" href="../../docentes/pages/agregar.php">Docentes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="background-color: aliceblue; border: solid 1px;position: relative; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;" href="">Estudiantes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="background-color: red; border: solid 1px;position: relative; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;" href="#">Cerrar Sesion</a>
+            </li>
+        </ul>
+    </nav>
 
 <h1>ACTUALIZAR</h1>
 <?php
